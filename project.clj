e18b7e2ff10b3a4eb4507ce38b93f6ba1e1e7798
Martin <zxm@basecity.com>
2013-07-22 14:54:43 +1200

    release 0.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject simple-actor "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject simple-actor "0.1"
+  :description "simple actor"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[log4j "1.2.16"]])
