64ed1896e78a6d15af3cc35e592e86eca8338b94
Martin <zxm@basecity.com>
2013-08-15 13:47:27 +1200

    upgrade clojure to 1.5.1, and version to 0.1.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject simple-actor "0.1"
+(defproject simple-actor "0.1.1"
   :description "simple actor"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[log4j "1.2.16"]])
